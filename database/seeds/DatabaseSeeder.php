016b79bea54ca9f95acbcaf1550e2aee82244eb8
Gilles <gilles.audemard@gmail.com>
2019-07-08 14:03:40 +0200

    video laravel 6

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,42 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::table('photos')->insert([
+            'title' => "citron",
+            'url' => "/images/portfolio/1.jpg",
+            'votes' => 230
+        ]);
+
+
+        DB::table('photos')->insert([
+            'title' => "mains",
+            'url' => "/images/portfolio/2.jpg",
+            'votes' => 23
+        ]);
+
+        DB::table('photos')->insert([
+            'title' => "balcons",
+            'url' => "/images/portfolio/3.jpg",
+            'votes' => 1540
+        ]);
+
+        DB::table('photos')->insert([
+            'title' => "thé",
+            'url' => "/images/portfolio/4.jpg",
+            'votes' => 45
+        ]);
+
+        DB::table('photos')->insert([
+            'title' => "auto portrait",
+            'url' => "/images/portfolio/5.jpg",
+            'votes' => 21
+        ]);
+
+        DB::table('photos')->insert([
+            'title' => "livres",
+            'url' => "/images/portfolio/6.jpg",
+            'votes' => 45
+        ]);
+
     }
 }
