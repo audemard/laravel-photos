d3083a1750234d344760ef6cde0b0fc966642822
Gilles <gilles.audemard@gmail.com>
2019-07-08 14:05:11 +0200

    video laravel 7

--- app/Http/Controllers/FirstController.php
+++ app/Http/Controllers/FirstController.php
@@ -2,13 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Photo;
 use Illuminate\Http\Request;
 
 class FirstController extends Controller
 {
     public function index() {
-        $photos = ["/images/portfolio/1.jpg", "/images/portfolio/2.jpg", "/images/portfolio/3.jpg", "/images/portfolio/4.jpg",
-        "/images/portfolio/5.jpg", "/images/portfolio/6.jpg"];
+        $photos = Photo::all();
         return view("firstcontroller.index", ["photos" => $photos]);
     }
 
--- resources/views/firstcontroller/index.blade.php
+++ resources/views/firstcontroller/index.blade.php
@@ -12,14 +12,14 @@
 
             @foreach($photos as $p)
                 <div class="mix col-md-6 col-lg-3 rened">
-                    <a href="{{ $p }}" class="portfolio-item pi-style2 set-bg"
-                       data-setbg="{{ $p }}">
+                    <a href="{{ $p->url }}" class="portfolio-item pi-style2 set-bg"
+                       data-setbg="{{ $p->url }}">
                         <div class="pi-inner">
                             <h2>+ See Project</h2>
                         </div>
                     </a>
                     <div class="portfolio-meta">
-                        <h2>Brand Campaign</h2>
+                        <h2>{{ $p->title }}</h2>
                         <p>Graphic design</p>
                     </div>
                 </div>
