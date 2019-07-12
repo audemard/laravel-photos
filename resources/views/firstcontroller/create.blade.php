1c5b1a49c704c6258a132f95d81944f1231cae02
Gilles <gilles.audemard@gmail.com>
2019-07-12 09:36:25 +0200

    video laravel 13

--- resources/views/firstcontroller/create.blade.php
+++ resources/views/firstcontroller/create.blade.php
@@ -8,14 +8,14 @@
 
     <section class="page-section">
         <div class="container">
-            <form class="contact-form" method="post" action="/photos">
+            <form class="contact-form" method="post" action="/photos" enctype="multipart/form-data">
                 @csrf
                 <div class="row">
                     <div class="col-md-6">
                         <input type="text" placeholder="Title" name="title">
                     </div>
                     <div class="col-md-6">
-                        <input type="text" placeholder="URL" name="url">
+                        <input type="file" placeholder="image" name="image">
                     </div>
                 </div>
                 <div class="text-center">
